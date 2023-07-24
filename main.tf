c9bed44997e3e3e9c96cd3fab4a97563183b312e
MeenakshyChatGPT <128809845+MeenakshyChatGPT@users.noreply.github.com>
2023-07-24 14:53:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
-
+terraform {
+  required_providers {
+    azurerm={
+        source="hashicorp/azurerm"
+        version="3.65.0"
+    }
+  }
+}
 provider "azurerm" {
   use_msi = true
   client_id       = var.client_id
