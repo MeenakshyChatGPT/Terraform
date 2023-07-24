4f504727d29b21dcf81240cf7b48d008a5125088
meenakshysivaraman88 <meenakshysivaraman88@gmail.com>
2023-07-24 18:37:37 +0530

    push changes

--- terraform-Azure-VM/main.tf
+++ terraform-Azure-VM/main.tf
@@ -9,10 +9,7 @@ terraform {
 }
 
 provider "azurerm" {
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
+  use_oidc            = true
   features{
   }
 }
