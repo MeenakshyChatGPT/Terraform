0a6165207d88007d84b9566610e253a8e41be2ed
MeenakshyChatGPT <128809845+MeenakshyChatGPT@users.noreply.github.com>
2023-07-24 19:51:55 +0530

    Update main.tf

--- terraform-Azure-VM/main.tf
+++ terraform-Azure-VM/main.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = ">= 1.5"
   required_providers {
     azurerm={
         source="hashicorp/azurerm"
-        version="3.61.0"
+        version="3.0.0"
     }
   }
 }
